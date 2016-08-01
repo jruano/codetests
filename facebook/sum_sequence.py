7ada2490a94fc8d7aecfe713c7aa59eb452828cc
jruano <jeruano@us.ibm.com>
2016-08-01 10:18:16 -0500

    Initial test code drop

---
+++ facebook/sum_sequence.py
@@ -0,0 +1,35 @@
+def check_sequence_total(nums, total):
+    running_index = 0
+
+    for num in nums:
+
+        # This is the first element of the set
+        if running_index == 0:
+            running_index += 1
+            continue
+
+        # Look for an equal sum for all nums leading up to this index
+        index = running_index
+        running_total = num
+        while index != 0:
+            running_total += nums[index - 1]
+            if running_total == total:
+                return True
+
+            index -= 1
+
+        running_index += 1
+
+    # Sequence sum equal to total was never found
+    return False
+
+
+if __name__ == "__main__":
+    nums = [23, 5, 4, 7, 2, 11]
+    total = 20
+
+    isSequenceSum = check_sequence_total(nums, total)
+    if isSequenceSum:
+        print "Why yes it is"
+    else:
+        print "No it is not"
---
+++ facebook/sum_zero.py
@@ -0,0 +1,47 @@
+def check_sum_zero(nums):
+    # initialize starting point
+    current_index = 0
+    inner_index = 1
+    outer_index = 2
+    outer_bound = len(nums)
+
+    # Total numbers we are checking is 3
+    # Xnn pattern
+    while current_index < outer_bound - 2:
+        for num in nums:
+            # nXn pattern
+            while inner_index < outer_bound - 1:
+                # Check simplest, sequential case
+                if num + nums[inner_index] + nums[outer_index] == 0:
+                    print str(num) + "," + str(nums[inner_index]) + "," + str(nums[outer_index])
+                    return True
+
+                # nnX pattern
+                outer_index += 1
+                while outer_index < outer_bound:
+                    if num + nums[inner_index] + nums[outer_index] == 0:
+                        print str(num) + "," + str(nums[inner_index]) + "," + str(nums[outer_index])
+                        return True
+
+                    outer_index += 1
+
+                # Update positions
+                inner_index += 1
+                outer_index = inner_index + 1
+
+            # Update positions
+            current_index += 1
+            inner_index = current_index + 1
+            outer_index = inner_index + 1
+
+    # Didn't find a zero sum
+    return False
+
+if __name__ == "__main__":
+    nums = [4, 2, -1, 1, 7, -12, -4]
+
+    isZeroSum = check_sum_zero(nums)
+    if isZeroSum:
+        print "Why yes it is"
+    else:
+        print "No it is not"
